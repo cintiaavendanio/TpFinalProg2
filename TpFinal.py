b3b8c858925b44a2bbaedb77dc0435022f5cf1c6
JoaquinRodriguez <joacorod@live.com>
2023-06-07 11:11:00 -0300

    joaco

--- TpFinal.py
+++ TpFinal.py
@@ -6,6 +6,7 @@ class ProgramaPrincipal:
     def menu(self):
         while True:
             print("probando")
+            print("soy joaco")
             print("Menu de opciones Buscalibre")
             print("1 - Cargar Libros")
             print("2 - Modificar precio de un libro")
