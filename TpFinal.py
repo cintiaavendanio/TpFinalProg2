2ee71c0e583482cf1e2c1abfd77617d415aa1eee
MateoBernardi5 <mateoobernardi@gmail.com>
2023-06-13 20:32:07 -0300

    Agregando Conexiones y Menu Principal

--- TpFinal.py
+++ TpFinal.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 
 class Conexiones:
@@ -10,99 +11,6 @@ class Conexiones:
 
     def cerrarConexion(self):
         self.miConexion.close()
-
-
-class Libreria:
-    def agregar_libro(
-        self, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles
-    ):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "INSERT INTO LIBROS (ISBN, titulo, autor, genero, precio,fechaUltimoPrecio, cantDisponibles) VALUES (?, ?, ?, ?, ?, ?,?)",
-                (
-                    ISBN,
-                    titulo,
-                    autor,
-                    genero,
-                    precio,
-                    fechaUltimoPrecio,
-                    cantDisponibles,
-                ),
-            )
-            conexion.miConexion.commit()
-            print("Libro agregado exitosamente")
-        except Exception as err:
-            print("Error al agregar un libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
-    def modificar_libro(self, ISBN_ingresado, nuevo_precio):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET precio = ? WHERE ISBN = ?",
-                (nuevo_precio, ISBN_ingresado),
-            )
-            conexion.miConexion.commit()
-            print("Libro modificado correctamente")
-        except Exception as err:
-            print("Error al modificar un libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
-    def borrar_libro(self, ISBN_ingresado):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "DELETE FROM LIBROS WHERE ISBN = ?",
-                (ISBN_ingresado,),
-            )
-            conexion.miConexion.commit()
-            print("Libro borrado correctamente")
-        except Exception as err:
-            print("Error al borrar libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
-    def modificar_disponibilidad(self, ISBN_ingresado, nueva_disponibilidad):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET cantDisponibles = ? WHERE ISBN = ?",
-                (nueva_disponibilidad, ISBN_ingresado),
-            )
-            conexion.miConexion.commit()
-            print("Cantidad modificada correctamente")
-        except Exception as err:
-            print("Error al modificar la cantidad de libros disponibles")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
-    def mostrar_libros(self):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            cursor = conexion.miCursor.execute("SELECT ISBN, titulo, autor FROM LIBROS")
-            for fila in cursor:
-                print("ISBN:", fila[0])
-                print("Título:", fila[1])
-                print("Autor:", fila[2])
-                print("---")
-        except Exception as err:
-            print("Error al mostrar libros")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
 
 class ProgramaPrincipal:
     def menu(self):
@@ -118,60 +26,82 @@ class ProgramaPrincipal:
             print("8 - Mostrar registro")
             print("99 - Crear tablas")
             print("0- Salir del menú")
-            nro = int(input("Por favor ingrese la opción mediante su número "))
+            try:
+                nro = int(input("Por favor ingrese la opción mediante su número: "))
+            except ValueError:
+                print("Por favor ingrese un número válido.")
+                continue
 
             if nro == 1:
-                ISBN = input("Por favor ingrese el ISBN del libro: ")
-                titulo = input("Por favor ingrese el titulo del libro: ")
-                autor = input("Por favor ingrese el autor del libro: ")
-                genero = input("Por favor ingrese el genero del libro: ")
-                precio = input("Por favor ingrese el precio del libro: ")
-                fechaUltimoPrecio = input(
-                    "Por favor ingrese la fecha de ultimo precio: "
-                )
-                cantDisponibles = input(
-                    "Por favor ingrese la cantidad de unidades disponibles: "
-                )
+                try:
+                    ISBN = int(input("Por favor ingrese el ISBN del libro: "))
+                    titulo = input("Por favor ingrese el titulo del libro: ")
+                    autor = input("Por favor ingrese el autor del libro: ")
+                    genero = input("Por favor ingrese el genero del libro: ")
+                    precio = float(input("Por favor ingrese el precio del libro: "))
+                    fechaUltimoPrecio = datetime.now()
+                    cantDisponibles = int(input("Por favor ingrese la cantidad de unidades disponibles: "))
 
-                libreria.agregar_libro(
-                    ISBN,
-                    titulo,
-                    autor,
-                    genero,
-                    precio,
-                    fechaUltimoPrecio,
-                    cantDisponibles,
-                )
+                    libreria.agregar_libro(
+                        ISBN,
+                        titulo,
+                        autor,
+                        genero,
+                        precio,
+                        fechaUltimoPrecio,
+                        cantDisponibles,
+                    )
+                
+                except Exception as err:
+                    print("Error al agregar un libro")
+                    print(err)
 
             elif nro == 2:
-                ISBN_ingresado = input("Por favor ingrese el ISBN del libro: ")
+                id = input("Por favor ingrese el id del libro: ")
                 nuevo_precio = input("Por favor ingrese el nuevo precio: ")
-                libreria.modificar_libro(ISBN_ingresado, nuevo_precio)
+                libreria.modificar_libro(id, nuevo_precio)
 
             elif nro == 3:
-                ISBN_ingresado = input("Por favor ingrese el ISBN del libro: ")
-                libreria.borrar_libro(ISBN_ingresado)
+                id = input("Por favor ingrese el id del libro: ")
+                libreria.borrar_libro(id)
 
             elif nro == 4:
-                ISBN_ingresado = input("Por favor ingrese el ISBN del libro: ")
+                id = input("Por favor ingrese el id del libro: ")
                 nueva_disponibilidad = input(
                     "Por favor ingrese la nueva disponibilidad: "
                 )
-                libreria.modificar_disponibilidad(ISBN_ingresado, nueva_disponibilidad)
+                libreria.modificar_disponibilidad(id, nueva_disponibilidad)
 
             elif nro == 5:
                 libreria.mostrar_libros()
 
+            elif nro == 6:
+                id_libro = input("ingrese id del libro vendido")
+                cantVendidas = input("ingrese cantidades vendidas")
+                fecha = datetime.now()
+
+                libreria.registrar_venta(id_libro, cantVendidas, fecha)
+
+            elif nro == 7:
+                porcentajeDeAumento = int(
+                    input("ingrese porcentaje de aumento (sin %)")
+                )
+                fechaActual = datetime.now()
+                print("fecha actual:")
+                print(fechaActual)
+                libreria.actualizarPrecios(fechaActual, porcentajeDeAumento)
+                
+            elif nro == 8:
+                fecha = input("ingrese fecha (AÑO-MES-DIA)")
+                libreria.mostrarRegistro(fecha)
+
             elif nro == 99:
                 self.crearTablas()
 
-            elif nro == 6:
-                ISBN = input("ingrese ISBN del libro vendido")
-                cantVendidas = input("ingrese cantidades vendidas")
-                fecha = input("ingrese fecha de venta")
-
             elif nro == 0:
                 break
+            else:
+                print("Opción inválida. Por favor ingrese un número válido.")
 
     def crearTablas(self):
         print("entro al crear tabla")
@@ -179,14 +109,16 @@ class ProgramaPrincipal:
         conexion.abrirConexion()
         conexion.miCursor.execute("DROP TABLE IF EXISTS LIBROS")
         conexion.miCursor.execute(
-            "CREATE TABLE LIBROS (id_libro INTEGER PRIMARY KEY,ISBN INTEGER, titulo VARCHAR(30), autor VARCHAR(30), genero VARCHAR(30), precio FLOAT NOT NULL, fechaUltimoPrecio INTEGER, cantDisponibles INTEGER NOT NULL, UNIQUE(ISBN), UNIQUE(titulo, autor))"
+            "CREATE TABLE LIBROS (id_libro INTEGER PRIMARY KEY, ISBN INTEGER, titulo VARCHAR(30), autor VARCHAR(30), genero VARCHAR(30), precio FLOAT NOT NULL, fechaUltimoPrecio DATETIME, cantDisponibles INTEGER NOT NULL, UNIQUE(ISBN), UNIQUE(titulo, autor))"
         )
-
+        conexion.miCursor.execute("DROP TABLE IF EXISTS VENTAS")
+        conexion.miCursor.execute(
+            "CREATE TABLE VENTAS (id_venta INTEGER PRIMARY KEY,id_libro INTEGER, cantVendida INTEGER, fecha INTEGER)"
+        )
+        conexion.miCursor.execute("DROP TABLE IF EXISTS HISTORICO_LIBROS")
+        conexion.miCursor.execute(
+            "CREATE TABLE HISTORICO_LIBROS (id_historico INTEGER PRIMARY KEY, id_libro, ISBN INTEGER, titulo VARCHAR(30), autor VARCHAR(30), genero VARCHAR(30), precio FLOAT NOT NULL, fechaUltimoPrecio DATETIME, cantDisponibles INTEGER NOT NULL)"
+        )
         conexion.miConexion.commit()
         conexion.cerrarConexion()
         print("Tablas creadas")
-
-
-libreria = Libreria()
-programa = ProgramaPrincipal()
-programa.menu()
