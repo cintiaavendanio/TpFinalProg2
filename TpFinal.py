ebdb80aeadc306f13ae51b4343edfb41807ce029
cinti.av <cin899a@gmail.com>
2023-06-13 20:37:33 -0300

    acomodando

--- TpFinal.py
+++ TpFinal.py
@@ -12,6 +12,7 @@ class Conexiones:
     def cerrarConexion(self):
         self.miConexion.close()
 
+
 class ProgramaPrincipal:
     def menu(self):
         while True:
@@ -40,7 +41,9 @@ class ProgramaPrincipal:
                     genero = input("Por favor ingrese el genero del libro: ")
                     precio = float(input("Por favor ingrese el precio del libro: "))
                     fechaUltimoPrecio = datetime.now()
-                    cantDisponibles = int(input("Por favor ingrese la cantidad de unidades disponibles: "))
+                    cantDisponibles = int(
+                        input("Por favor ingrese la cantidad de unidades disponibles: ")
+                    )
 
                     libreria.agregar_libro(
                         ISBN,
@@ -51,7 +54,7 @@ class ProgramaPrincipal:
                         fechaUltimoPrecio,
                         cantDisponibles,
                     )
-                
+
                 except Exception as err:
                     print("Error al agregar un libro")
                     print(err)
@@ -90,7 +93,7 @@ class ProgramaPrincipal:
                 print("fecha actual:")
                 print(fechaActual)
                 libreria.actualizarPrecios(fechaActual, porcentajeDeAumento)
-                
+
             elif nro == 8:
                 fecha = input("ingrese fecha (AÑO-MES-DIA)")
                 libreria.mostrarRegistro(fecha)
@@ -122,188 +125,193 @@ class ProgramaPrincipal:
         conexion.miConexion.commit()
         conexion.cerrarConexion()
         print("Tablas creadas")
-        
+
     class Libreria:
-    def agregar_libro(
-        self, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles
-    ):
-        if (
-            not ISBN
-            or not titulo
-            or not autor
-            or not genero
-            or not precio
-            or not cantDisponibles
+        def agregar_libro(
+            self,
+            ISBN,
+            titulo,
+            autor,
+            genero,
+            precio,
+            fechaUltimoPrecio,
+            cantDisponibles,
         ):
-            print("Por favor ingrese todos los campos requeridos.")
-            return
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "INSERT INTO LIBROS (ISBN, titulo, autor, genero, precio,fechaUltimoPrecio, cantDisponibles) VALUES (?, ?, ?, ?, ?, ?,?)",
-                (
-                    ISBN,
-                    titulo,
-                    autor,
-                    genero,
-                    precio,
-                    fechaUltimoPrecio,
-                    cantDisponibles,
-                ),
-            )
-            conexion.miConexion.commit()
-            print("Libro agregado exitosamente")
-        except Exception as err:
-            print("Error al agregar un libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+            if (
+                not ISBN
+                or not titulo
+                or not autor
+                or not genero
+                or not precio
+                or not cantDisponibles
+            ):
+                print("Por favor ingrese todos los campos requeridos.")
+                return
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                conexion.miCursor.execute(
+                    "INSERT INTO LIBROS (ISBN, titulo, autor, genero, precio,fechaUltimoPrecio, cantDisponibles) VALUES (?, ?, ?, ?, ?, ?,?)",
+                    (
+                        ISBN,
+                        titulo,
+                        autor,
+                        genero,
+                        precio,
+                        fechaUltimoPrecio,
+                        cantDisponibles,
+                    ),
+                )
+                conexion.miConexion.commit()
+                print("Libro agregado exitosamente")
+            except Exception as err:
+                print("Error al agregar un libro")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def modificar_libro(self, id, nuevo_precio):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        consulta = "SELECT id_libro FROM LIBROS WHERE id_libro = ?"
-        cursor = conexion.miCursor.execute(consulta, (id,))
-        if not cursor.fetchone():
-            print("El libro con el ID especificado no existe.")
-            return
+        def modificar_libro(self, id, nuevo_precio):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            consulta = "SELECT id_libro FROM LIBROS WHERE id_libro = ?"
+            cursor = conexion.miCursor.execute(consulta, (id,))
+            if not cursor.fetchone():
+                print("El libro con el ID especificado no existe.")
+                return
 
-        try:
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET precio = ? WHERE id_libro = ?",
-                (nuevo_precio, id),
-            )
-            conexion.miConexion.commit()
-            print("Libro modificado correctamente")
-        except Exception as err:
-            print("Error al modificar un libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+            try:
+                conexion.miCursor.execute(
+                    "UPDATE LIBROS SET precio = ? WHERE id_libro = ?",
+                    (nuevo_precio, id),
+                )
+                conexion.miConexion.commit()
+                print("Libro modificado correctamente")
+            except Exception as err:
+                print("Error al modificar un libro")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def borrar_libro(self, id):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "DELETE FROM LIBROS WHERE id_libro = ?",
-                (id,),
-            )
-            conexion.miConexion.commit()
-            print("Libro borrado correctamente")
-        except Exception as err:
-            print("Error al borrar libro")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+        def borrar_libro(self, id):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                conexion.miCursor.execute(
+                    "DELETE FROM LIBROS WHERE id_libro = ?",
+                    (id,),
+                )
+                conexion.miConexion.commit()
+                print("Libro borrado correctamente")
+            except Exception as err:
+                print("Error al borrar libro")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def modificar_disponibilidad(self, id, nueva_disponibilidad):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET cantDisponibles = ? WHERE id_libro = ?",
-                (nueva_disponibilidad, id),
-            )
-            conexion.miConexion.commit()
-            print("Cantidad modificada correctamente")
-        except Exception as err:
-            print("Error al modificar la cantidad de libros disponibles")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+        def modificar_disponibilidad(self, id, nueva_disponibilidad):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                conexion.miCursor.execute(
+                    "UPDATE LIBROS SET cantDisponibles = ? WHERE id_libro = ?",
+                    (nueva_disponibilidad, id),
+                )
+                conexion.miConexion.commit()
+                print("Cantidad modificada correctamente")
+            except Exception as err:
+                print("Error al modificar la cantidad de libros disponibles")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def mostrar_libros(self):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            cursor = conexion.miCursor.execute(
-                "SELECT id_libro, titulo, autor FROM LIBROS ORDER BY id_libro, autor, titulo"
-            )
-            for fila in cursor:
-                print("id:", fila[0])
-                print("Título:", fila[1])
-                print("Autor:", fila[2])
-                print("---")
-        except Exception as err:
-            print("Error al mostrar libros")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+        def mostrar_libros(self):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                cursor = conexion.miCursor.execute(
+                    "SELECT id_libro, titulo, autor FROM LIBROS ORDER BY id_libro, autor, titulo"
+                )
+                for fila in cursor:
+                    print("id:", fila[0])
+                    print("Título:", fila[1])
+                    print("Autor:", fila[2])
+                    print("---")
+            except Exception as err:
+                print("Error al mostrar libros")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def registrar_venta(self, id_libro, cantVendidas, fecha):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "INSERT INTO VENTAS (id_libro, cantVendida,fecha) VALUES (?, ?, ?)",
-                (id_libro, cantVendidas, fecha),
-            )
+        def registrar_venta(self, id_libro, cantVendidas, fecha):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                conexion.miCursor.execute(
+                    "INSERT INTO VENTAS (id_libro, cantVendida,fecha) VALUES (?, ?, ?)",
+                    (id_libro, cantVendidas, fecha),
+                )
 
-            cursor = conexion.miCursor.execute(
-                "SELECT cantDisponibles FROM LIBROS WHERE id_libro = ?",
-                (id_libro),
-            )
-            for fila in cursor:
-                cantDisponibles = fila[0] - int(cantVendidas)
+                cursor = conexion.miCursor.execute(
+                    "SELECT cantDisponibles FROM LIBROS WHERE id_libro = ?",
+                    (id_libro),
+                )
+                for fila in cursor:
+                    cantDisponibles = fila[0] - int(cantVendidas)
 
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET cantDisponibles = ? WHERE id_libro = ?",
-                (cantDisponibles, id_libro),
-            )
+                conexion.miCursor.execute(
+                    "UPDATE LIBROS SET cantDisponibles = ? WHERE id_libro = ?",
+                    (cantDisponibles, id_libro),
+                )
 
-            print("Venta agregada exitosamente")
+                print("Venta agregada exitosamente")
 
-            conexion.miConexion.commit()
-        except Exception as err:
-            print("Error al agregar la venta")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+                conexion.miConexion.commit()
+            except Exception as err:
+                print("Error al agregar la venta")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def actualizarPrecios(self, fechaActual, porcentajeDeAumento):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            conexion.miCursor.execute(
-                "INSERT INTO HISTORICO_LIBROS (id_libro, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles) SELECT id_libro, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles FROM LIBROS"
-            )
-            print("Libros agregados a la tabla historico exitosamente")
-            aumento = float((porcentajeDeAumento / 100) + 1)
-            conexion.miCursor.execute(
-                "UPDATE LIBROS SET precio = precio*?, fechaUltimoPrecio = ?",
-                (aumento, fechaActual),
-            )
+        def actualizarPrecios(self, fechaActual, porcentajeDeAumento):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                conexion.miCursor.execute(
+                    "INSERT INTO HISTORICO_LIBROS (id_libro, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles) SELECT id_libro, ISBN, titulo, autor, genero, precio, fechaUltimoPrecio, cantDisponibles FROM LIBROS"
+                )
+                print("Libros agregados a la tabla historico exitosamente")
+                aumento = float((porcentajeDeAumento / 100) + 1)
+                conexion.miCursor.execute(
+                    "UPDATE LIBROS SET precio = precio*?, fechaUltimoPrecio = ?",
+                    (aumento, fechaActual),
+                )
 
-            conexion.miConexion.commit()
-        except Exception as err:
-            print("Error al agregar a historicos")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
+                conexion.miConexion.commit()
+            except Exception as err:
+                print("Error al agregar a historicos")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
-    def mostrarRegistro(self, fecha):
-        conexion = Conexiones()
-        conexion.abrirConexion()
-        try:
-            consulta = "SELECT id_libro, ISBN, titulo, fechaUltimoPrecio FROM LIBROS WHERE fechaUltimoPrecio < ?"
-            cursor = conexion.miCursor.execute(consulta, (fecha,))
-            for fila in cursor:
-                print("id:", fila[0])
-                print("ISBN:", fila[1])
-                print("titulo:", fila[2])
-                print("fechaUltimoPrecio:", fila[3])
-                print("---")
+        def mostrarRegistro(self, fecha):
+            conexion = Conexiones()
+            conexion.abrirConexion()
+            try:
+                consulta = "SELECT id_libro, ISBN, titulo, fechaUltimoPrecio FROM LIBROS WHERE fechaUltimoPrecio < ?"
+                cursor = conexion.miCursor.execute(consulta, (fecha,))
+                for fila in cursor:
+                    print("id:", fila[0])
+                    print("ISBN:", fila[1])
+                    print("titulo:", fila[2])
+                    print("fechaUltimoPrecio:", fila[3])
+                    print("---")
 
-            conexion.miConexion.commit()
-        except Exception as err:
-            print("Error al mostrar registros")
-            print(err)
-        finally:
-            conexion.cerrarConexion()
-
-
+                conexion.miConexion.commit()
+            except Exception as err:
+                print("Error al mostrar registros")
+                print(err)
+            finally:
+                conexion.cerrarConexion()
 
 
 libreria = Libreria()
